103f0f1e0d31b82af4ab2a65792344da4ef72d57
hafezhammamy <h.hammamy@freightos.com>
2024-10-25 22:36:49 +0300

    support video

--- src/pages/home/components/HeroHome.jsx
+++ src/pages/home/components/HeroHome.jsx
@@ -6,6 +6,8 @@ import StyledTypography from "layout/components/StyledTypography";
 import { tokens } from "locales/tokens";
 import { useTranslation } from "react-i18next";
 
+import video from "assets/our_brands.mp4";
+
 const heroTextStyle = {
   paddingBottom: 0,
   display: "block",
@@ -30,6 +32,14 @@ const heroButton = {
   },
 };
 
+const videoStyle = {
+  position: "absolute",
+  right: 0,
+  bottom: 0,
+  minWidth: "100%",
+  minHeight: "100%",
+};
+
 export const HomeHero = () => {
   const { t } = useTranslation();
   return (
@@ -38,14 +48,25 @@ export const HomeHero = () => {
         backgroundRepeat: "no-repeat",
         backgroundPosition: "top center",
         backgroundColor: "rgba(0, 0, 0, 0.2)",
-        backgroundImage: 'url("/assets/hero-cover.webp")',
-        background:
-          "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('/assets/hero-cover.webp')",
-        height: "80vh",
-        pt: "120px",
+        background: "linear-gradient(145deg, #ff77a9, #d3577f)",
+        padding: 0,
+        overflow: "hidden",
+        height: "90vh",
       }}
     >
-      <Container maxWidth="xxl" sx={{ position: "relative", height: "100%" }}>
+      <Container
+        maxWidth="xxl"
+        sx={{
+          position: "relative",
+          height: "100%",
+          padding: "0 !important",
+          margin: 0,
+        }}
+      >
+        <video autoPlay loop muted playsInline style={{ videoStyle }}>
+          <source src={video} type="video/mp4" />
+        </video>
+
         <Box maxWidth="xl" sx={heroTextStyle}>
           <Box sx={{ width: "50%" }}>
             <Stack spacing={3}>
