d0d9b571022db62670afa54f2206af3011798a16
hafezhammamy <h.hammamy@freightos.com>
2024-10-26 14:27:28 +0200

    add juvelookbrand

--- src/pages/home/components/brands/JuvelookBrand.jsx
+++ src/pages/home/components/brands/JuvelookBrand.jsx
@@ -1,5 +1,37 @@
+import { Button } from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import video from "assets/ksugery.mp4";
+import StyledTypography, {
+  QuoteTypography,
+} from "layout/components/StyledTypography";
+import { buttonStyle, textStyle, videoStyle } from "./styles";
+
 const JuvelookBrand = (props) => {
-  return <div></div>;
+  return (
+    <>
+      <video autoPlay loop muted playsInline style={videoStyle}>
+        <source src={video} type="video/mp4" />
+      </video>
+      <Box maxWidth="xl" sx={textStyle}>
+        <Box sx={{ width: "50%" }}>
+          <Stack spacing={3}>
+            <QuoteTypography variant="h6">Juvelook</QuoteTypography>
+            <StyledTypography variant="h5">
+              Experience Juvelook: Our unique PLA filler enhances your
+              appearance with precision. Small PDLLA particles are injected into
+              the dermis, evenly volumizing skin by boosting collagen
+              production.
+            </StyledTypography>
+            <Box>
+              <Button sx={buttonStyle} size="large" variant="outlined">
+                DISCOVER BRAND
+              </Button>
+            </Box>
+          </Stack>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 JuvelookBrand.propTypes = {};
