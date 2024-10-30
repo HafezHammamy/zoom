96f4bebd752b5f0c34938869c06e4c649ff85768
hafezhammamy <hafez-hammamy@live.com>
2024-10-30 18:30:57 +0200

    fix about us banner

--- src/pages/home/components/AboutUsBanner.jsx
+++ src/pages/home/components/AboutUsBanner.jsx
@@ -11,80 +11,70 @@ import logo from "assets/logo.png";
 const AboutUsBanner = (props) => {
   const { t } = useTranslation();
   return (
-    <Box
+    <Container
+      maxWidth="xxl"
       sx={{
         backgroundColor: "#eee8f5",
+        position: "relative",
+        height: "100%",
       }}
     >
-      <Container
-        maxWidth="xxl"
-        sx={{
-          position: "relative",
-          height: "100%",
-          padding: 10,
-          margin: 0,
-        }}
-      >
-        <Grid container spacing={2}>
-          <Grid size={5} sx={{ position: "relative" }}>
-            <Box
-              component="img"
-              src={image2}
-              alt="About Us"
-              sx={{
-                position: "absolute",
-                height: "600px",
-                borderRadius: 2,
-                right: -100,
-                top: 100,
-                zIndex: 1,
-              }}
-            />
-          </Grid>
-          <Grid size={7}>
-            <Box
-              sx={{
-                backgroundRepeat: "no-repeat",
-                backgroundImage: `url(${image})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                height: "85vh",
-                borderRadius: 2,
-                color: "white",
-              }}
-            >
-              <Box>
-                <Stack direction={"row"} justifyContent="center">
-                  <Stack
-                    spacing={5}
-                    direction={"column"}
-                    sx={{ width: "50%", marginTop: "20%" }}
-                  >
-                    <Typography variant="h2">
-                      {t(tokens.about.title)}
-                    </Typography>
-                    <Typography variant="body2">
-                      {t(tokens.about.paragraph)}
-                    </Typography>
-                    <Button sx={buttonStyle} size="large" variant="outlined">
-                      {t(tokens.common.buttons.readMore)}
-                    </Button>
-                  </Stack>
-                </Stack>
-              </Box>
-            </Box>
-          </Grid>
-          <Grid size={12}>
-            <Box sx={{ marginLeft: "270px", marginTop: "50px" }}>
-              <img style={{ height: "50px" }} src={logo} alt={"Dynamics"} />
-            </Box>
-          </Grid>
+      <Grid container spacing={2} alignItems="stretch">
+        <Grid size={5} sx={{ position: "relative" }}>
+          <Box
+            component="img"
+            src={image2}
+            alt="About Us"
+            sx={{
+              position: "absolute",
+              height: "600px",
+              borderRadius: 2,
+              right: -100,
+              top: 100,
+              zIndex: 1,
+            }}
+          />
         </Grid>
-      </Container>
-    </Box>
+        <Grid size={7}>
+          <Box
+            sx={{
+              backgroundRepeat: "no-repeat",
+              backgroundImage: `url(${image})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              borderRadius: 2,
+              color: "white",
+            }}
+          >
+            <Stack direction={"row"} justifyContent="center">
+              <Stack
+                spacing={5}
+                direction={"column"}
+                sx={{
+                  padding: "20%",
+                  boxSizing: "content-box",
+                  minHeight: "50vh",
+                }}
+              >
+                <Typography variant="h2">{t(tokens.about.title)}</Typography>
+                <Typography variant="body2">
+                  {t(tokens.about.paragraph)}
+                </Typography>
+                <Button sx={buttonStyle} size="large" variant="outlined">
+                  {t(tokens.common.buttons.readMore)}
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+        </Grid>
+        <Grid offset={1} size={11}>
+          <Box sx={{ marginTop: "50px", marginBottom: "50px" }}>
+            <img style={{ height: "50px" }} src={logo} alt={"Dynamics"} />
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
-
-AboutUsBanner.propTypes = {};
 
 export default AboutUsBanner;
