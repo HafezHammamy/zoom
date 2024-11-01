78a73d0b08024c597bb65ba11d2d9301444f25b7
hafezhammamy <hafez-hammamy@live.com>
2024-11-01 14:07:48 +0200

    add new images

--- src/pages/about/AboutUsPage.jsx
+++ src/pages/about/AboutUsPage.jsx
@@ -8,6 +8,10 @@ import AboutSection from "./components/AboutSection";
 import { AboutUsHero } from "./components/AboutUsHero";
 import { ProductBanner } from "./components/ProductBanner";
 import ValuesBanner from "./components/ValuesBanner";
+import image1 from "assets/dynamics123.png";
+import image2 from "assets/dynamicshpp.png";
+import image3 from "assets/dfghkl.png";
+import image4 from "assets/gghhjkl.png";
 
 const AboutUsPage = (props) => {
   usePageView();
@@ -24,20 +28,24 @@ const AboutUsPage = (props) => {
               reverse={true}
               title={t(tokens.about.title)}
               paragraph={t(tokens.about.page.card1)}
+              passedImage={image1}
             />
             <AboutSection
               title={t(tokens.about.page.card2.title)}
               paragraph={t(tokens.about.page.card2.paragraph)}
+              passedImage={image2}
             />
             <ValuesBanner />
             <AboutSection
               reverse={true}
               title={t(tokens.about.page.card3.title)}
               paragraph={t(tokens.about.page.card3.paragraph)}
+              passedImage={image3}
             />
             <AboutSection
               title={t(tokens.about.page.card4.title)}
               paragraph={t(tokens.about.page.card4.paragraph)}
+              passedImage={image4}
             />
           </Container>
         </Box>
