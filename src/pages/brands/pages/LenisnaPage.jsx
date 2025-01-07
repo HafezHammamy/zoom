ba908b6124dbeec7bf3aea793033f152c0b4414c
hafezhammamy <hafez-hammamy@live.com>
2025-01-07 14:10:52 +0200

    feat: update video source and adjust LenisnaPage layout components

--- src/pages/brands/pages/LenisnaPage.jsx
+++ src/pages/brands/pages/LenisnaPage.jsx
@@ -4,7 +4,7 @@ import heroProductImage from "assets/lenisna/lenisna-hero-girl.png";
 import productImage from "assets/lenisna/lenisnaProduct.png";
 import girlImage from "assets/lenisna/lenisnaGirl.png";
 import image from "assets/lenisna/lenisnaHeroImage.png";
-import video from "assets/our_brands.mp4";
+import video from "assets/lenisna/hero_video.mp4";
 import { usePageView } from "hooks/use-page-view";
 import useScrollToTop from "hooks/useScrollToTop";
 import { ImageHero } from "layout/components/ImageHero";
@@ -29,7 +29,6 @@ const LenisnaPage = (props) => {
           description={t(tokens.brands.lenisna.description)}
           videoSrc={video}
           actionPath="#"
-          actionLabel={t(tokens.common.buttons.readMore)}
         />
 
         <ImageHero
@@ -37,57 +36,61 @@ const LenisnaPage = (props) => {
           title={t(tokens.brands.lenisna.whatIs)}
           description={t(tokens.brands.lenisna.whatIsDescription)}
           actionPath="#"
-          actionLabel={t(tokens.common.buttons.readMore)}
+          darken={false}
         />
-        <Box maxWidth="xxl" sx={{ backgroundColor: "#E9C1CA" }}>
-          <Container maxWidth="xxl">
-            <AboutSection
-              backgroundColor="#E9C1CA"
-              title2={t(tokens.brands.lenisna.results)}
-              paragraph2={
-                <>
-                  <ul>
-                    <li>{t(tokens.brands.lenisna.result1)}</li>
-                    <li>{t(tokens.brands.lenisna.result2)}</li>
-                    <li>{t(tokens.brands.lenisna.result3)}</li>
-                  </ul>
-                </>
-              }
-              backImage={background_green}
-              title={t(tokens.brands.lenisna.application)}
-              paragraph={t(tokens.brands.lenisna.applicationDescription)}
-              passedImage={girlImage}
-              hideAction
-            />
-          </Container>
-        </Box>
+        {false && (
+          <Box maxWidth="xxl" sx={{ backgroundColor: "#E9C1CA" }}>
+            <Container maxWidth="xxl">
+              <AboutSection
+                backgroundColor="#E9C1CA"
+                title2={t(tokens.brands.lenisna.results)}
+                paragraph2={
+                  <>
+                    <ul>
+                      <li>{t(tokens.brands.lenisna.result1)}</li>
+                      <li>{t(tokens.brands.lenisna.result2)}</li>
+                      <li>{t(tokens.brands.lenisna.result3)}</li>
+                    </ul>
+                  </>
+                }
+                backImage={background_green}
+                title={t(tokens.brands.lenisna.application)}
+                paragraph={t(tokens.brands.lenisna.applicationDescription)}
+                passedImage={girlImage}
+                hideAction
+              />
+            </Container>
+          </Box>
+        )}
 
-        <Box maxWidth="xxl" sx={{ backgroundColor: "#E9C1CA" }}>
-          <Container maxWidth="xxl">
-            <AboutSection
-              backgroundColor="#E9C1CA"
-              title={t(tokens.brands.lenisna.keyBenefits)}
-              paragraph={
-                <>
-                  <ul>
-                    <li>{t(tokens.brands.lenisna.keyBenefit1)}</li>
-                    <li>{t(tokens.brands.lenisna.keyBenefit2)}</li>
-                    <li>{t(tokens.brands.lenisna.keyBenefit3)}</li>
-                  </ul>
-                </>
-              }
-              passedImage={productImage}
-              reverse
-              hideAction
-            />
-          </Container>
-        </Box>
+        {false && (
+          <Box maxWidth="xxl" sx={{ backgroundColor: "#E9C1CA" }}>
+            <Container maxWidth="xxl">
+              <AboutSection
+                backgroundColor="#E9C1CA"
+                title={t(tokens.brands.lenisna.keyBenefits)}
+                paragraph={
+                  <>
+                    <ul>
+                      <li>{t(tokens.brands.lenisna.keyBenefit1)}</li>
+                      <li>{t(tokens.brands.lenisna.keyBenefit2)}</li>
+                      <li>{t(tokens.brands.lenisna.keyBenefit3)}</li>
+                    </ul>
+                  </>
+                }
+                passedImage={productImage}
+                reverse
+                hideAction
+              />
+            </Container>
+          </Box>
+        )}
         <ImageHero
           image={heroProductImage}
           title={t(tokens.brands.lenisna.howIs)}
           description={t(tokens.brands.lenisna.howIsDescription)}
           actionPath="#"
-          actionLabel={t(tokens.common.buttons.readMore)}
+          darken={false}
         />
       </main>
     </>
