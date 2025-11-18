14b89fb31b6efa3109b8a5b5580826b4b6499114
hafezhammamy <h.hammamy@freightos.com>
2025-11-18 23:45:11 +0200

    Refactor AboutUsPage by integrating product display and removing ProductBanner component, enhancing layout with new styling and improved responsiveness.

--- src/pages/about/AboutUsPage.jsx
+++ src/pages/about/AboutUsPage.jsx
@@ -1,21 +1,30 @@
-import { Box, Container } from "@mui/system";
+import { Box, Container, Grid, Stack } from "@mui/system";
 import { usePageView } from "hooks/use-page-view";
 import { Seo } from "layout/components/Seo";
 import { tokens } from "locales/tokens";
 import { useTranslation } from "react-i18next";
 import { AboutUsHero } from "./components/AboutUsHero";
-import { ProductBanner } from "./components/ProductBanner";
 import ValuesBanner from "./components/ValuesBanner";
+import product from "assets/about.svg";
+import productRtl from "assets/aboutRtl.png";
 
 import background_dark from "assets/dynamics-gradient-dark.png";
 import background_green from "assets/dynamics-gradient-green.png";
 import useScrollToTop from "hooks/useScrollToTop";
 import DualSection from "./components/DualSection";
+import { Button, useTheme } from "@mui/material";
+import StyledTypography, {
+  QuoteTypography,
+} from "layout/components/StyledTypography";
+import { RouterLink } from "layout/components/router-link";
+import { paths } from "paths";
 
 const AboutUsPage = (props) => {
   usePageView();
   const { t } = useTranslation();
   useScrollToTop();
+  const theme = useTheme();
+  const isRtl = theme.direction === "rtl";
 
   return (
     <>
@@ -47,8 +56,117 @@ const AboutUsPage = (props) => {
               backImage2={background_green}
             />
           </Container>
+          <Container
+            maxWidth="xxl"
+            sx={{
+              backgroundImage: `url(${isRtl ? productRtl : product})`,
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "top center",
+              position: "relative",
+              height: "100%",
+              paddingTop: 5,
+              paddingBottom: 5,
+            }}
+          >
+            <Box maxWidth="xxl">
+              <Container maxWidth="xxl">
+                <Grid
+                  container
+                  spacing={2}
+                  alignItems="stretch"
+                  sx={{ padding: 5 }}
+                >
+                  <Grid size={{ xs: 12, md: 6 }}>
+                    <Box
+                      sx={{
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        color: "white",
+                      }}
+                    >
+                      <Stack direction="row" justifyContent="center">
+                        <Box
+                          sx={{
+                            width: { xs: "100%", md: "80%" },
+                            minHeight: { md: "660px" },
+                            display: "flex",
+                          }}
+                        >
+                          <Stack
+                            spacing={5}
+                            direction="column"
+                            justifyContent="flex-start"
+                            alignItems="flex-start"
+                            sx={{
+                              height: "100%",
+                              width: "100%",
+                              px: { xs: 4, md: 6 },
+                              py: { xs: 6, md: 8 },
+                              pt: { xs: 6, md: 18 },
+                              textAlign: "left",
+                            }}
+                          >
+                            <Stack spacing={3} direction={"column"}>
+                              <Box
+                                maxWidth="xxl"
+                                sx={{
+                                  height: "100vh",
+                                  width: "100%",
+                                }}
+                              >
+                                <Stack
+                                  spacing={3}
+                                  sx={{
+                                    color: "#fff",
+                                    height: "100%",
+                                  }}
+                                  justifyContent="center"
+                                >
+                                  <QuoteTypography variant="body1">
+                                    {t(tokens.about.page.product.title)}
+                                  </QuoteTypography>
+                                  <StyledTypography variant="h2">
+                                    {t(tokens.about.page.product.subtitle)}
+                                  </StyledTypography>
+                                  <StyledTypography variant="body2">
+                                    {t(tokens.about.page.product.paragraph)}
+                                  </StyledTypography>
+                                  <Box>
+                                    <Button
+                                      LinkComponent={RouterLink}
+                                      href={paths.ourBrands}
+                                      sx={{
+                                        color: "white",
+                                        borderColor: "white",
+                                        height: "65px",
+                                        width: "281px",
+                                        "&:hover": {
+                                          borderColor: "white",
+                                          color: "#ab92e1",
+                                          backgroundColor: "white",
+                                        },
+                                      }}
+                                      size="large"
+                                      variant="outlined"
+                                    >
+                                      {t(tokens.common.buttons.readMore)}
+                                    </Button>
+                                  </Box>
+                                </Stack>
+                              </Box>
+                            </Stack>
+                          </Stack>
+                        </Box>
+                      </Stack>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Container>
+            </Box>
+          </Container>
         </Box>
-        <ProductBanner />
       </main>
     </>
   );
